1cc609527a05b98ec2f3a502dae7db5e4ee7fca2
Mihir Pujara <mihir.pujara@eclinicalworks.com>
2019-07-29 12:19:09 +0530

    Solved Kata upto step 7 i.e. multiple delimiters

---
+++ StringKata/Calculator.h
@@ -0,0 +1,17 @@
+//
+//  Calculator.h
+//  StringKata
+//
+//  Created by Mihir Pujara on 29/07/19.
+//  Copyright © 2019 Mihir Pujara. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Calculator : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ StringKata/Calculator.m
@@ -0,0 +1,13 @@
+//
+//  Calculator.m
+//  StringKata
+//
+//  Created by Mihir Pujara on 29/07/19.
+//  Copyright © 2019 Mihir Pujara. All rights reserved.
+//
+
+#import "Calculator.h"
+
+@implementation Calculator
+
+@end
